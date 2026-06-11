0d495065df311d538a7738a2451c19b4ac99f270
Florian Pilsl <bt706750@myubt.de>
2026-06-11 17:34:51 +0200

    some changes

--- src/test/java/org/campuscoffee/steps/SearchSteps.java
+++ src/test/java/org/campuscoffee/steps/SearchSteps.java
@@ -1,5 +1,7 @@
 package org.campuscoffee.steps;
 import io.cucumber.java.ParameterType;
+import io.cucumber.java.PendingException;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
@@ -17,7 +19,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class SearchSteps {
     Search search = new Search();
     CoffeStore output;
-
+    CoffeStore input;
     @ParameterType("true|false")
     public Boolean booleanValue(String value) {
         return Boolean.parseBoolean(value);
@@ -27,6 +29,7 @@ public class SearchSteps {
     public void before() {
          search = new Search();
          output = null;
+         input= null;
      }
 
     @Given("there are no registered CoffeShops")
@@ -38,6 +41,7 @@ public class SearchSteps {
     public void add_store_with_price(final String name, final String location, final boolean reuse, final String coffetype, final int price) {
         CoffeStore cs = new CoffeStore(name, org.campuscoffee.Location.valueOf(location.toUpperCase()),reuse);
         cs.setPrice(coffetype, price);
+        input=cs;
         search.addStore(cs);
     }
     @When("the price for {string} gets compared for {string} and {string}")
@@ -48,5 +52,18 @@ public class SearchSteps {
     public void checkOutput(final String storeName) {
         assertEquals(output.getName(), storeName);
     }
+    @Then ("the CoffeeShop with Name {string}, Location {string} and returnpoint {booleanValue} should appear")
+    public void  checkInput(final String storeName, final String location, boolean inside ){assertEquals(input.getName(),storeName);
+  assertTrue(search.getAllStores().stream().anyMatch(store->store.getName().equals(storeName)&&store.getLocation().equals(location.valueOf(location.toUpperCase())))&&store.getReturnPoint());
+    }
 
+    @And("it should be with the only one")
+    public void itShouldBeWithTheOnlyOne() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();//assertEquals(1,search.getAllStore().Size())
+    }
+
+
+
+    //...
 }
--- src/test/resources/features/search.feature
+++ src/test/resources/features/search.feature
@@ -5,3 +5,14 @@ Feature: Functions of Campus Coffee
     And a CoffeShop with Name "Cafeteria",Location "Uni" and returnpoint true and "Cappuccino" Price 4 is added to the System
     And the price for "Cappuccino" gets compared for "Crazy Sheep" and "Cafeteria"
     Then the System should show "Cafeteria" as the one with cheaper coffee
+
+    Scenario: Create CoffeeShop
+      Given there are no registered CoffeShops
+      When a CoffeShop with Name "CS",Location "Uni" and returnpoint true and "Cappuccino" Price 4 is added to the System
+      Then the CoffeeShop with Name "CS", Location "Uni" and returnpoint true should appear
+      And it should be with the only one
+      #Then the System should show "Hi" as a new shop
+
+    Scenario: Search CoffeeShop
+      Given there are  registered CoffeeShops
+      When Location "Uni"
